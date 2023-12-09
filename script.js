0b3010d87bf14f70187b37a68f6eb0603bbe2c85
IvanR96 <irios96@hotmail.com>
2023-12-08 23:14:36 -0800

    add book to library completed

--- script.js
+++ script.js
@@ -12,5 +12,18 @@ function Book(title, author, status){
 }
 
 function addBookToLibrary() {
-    
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let status = document.getElementById('status').value;
+
+
+    let newBook = new Book(title, author, status);
+
+    myLibrary.push(newBook);
+
+    console.log("Book added to the library:", newBook);
+
+    document.getElementById('bookForm').reset();
+
 }
+
